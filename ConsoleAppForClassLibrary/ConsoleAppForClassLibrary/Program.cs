180c415b93d581fdaf2359f471673795de94049b
Sumit Kumar <Sumit.Kumar3@honeywell.com>
2022-06-29 16:33:13 +0530

    Demo class and console app added

---
+++ ConsoleAppForClassLibrary/ConsoleAppForClassLibrary/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary;
+
+
+namespace ConsoleAppForClassLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo obj1 = new Demo();
+            obj1.message();
+            Console.Read();
+        }
+    }
+}
